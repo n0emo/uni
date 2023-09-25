fbce7a465f78630c75daefb25bcc5d2229335952
Albert Shefner <solaris945@gmail.com>
2023-09-26 00:20:52 +0300

    Added 3 term

---
+++ 3 term/Structures and Algorithms/Lab 1/sort.h
@@ -0,0 +1,27 @@
+#ifndef CPP_LAB_1_SORT_H
+#define CPP_LAB_1_SORT_H
+
+template<typename T>
+void selection_sort(T *begin, T *end, int (*cmp)(T a, T b));
+
+template<typename T>
+void bubble_sort(T *begin, T *end, int (*cmp)(T a, T b));
+
+template<typename T>
+T *merge_new_mem(T *begin_a, T *end_a, T *begin_b, T *end_b, int (*cmp)(T a, T b));
+
+template<typename T>
+void merge_middle(T *begin, T *end, int (*cmp)(T a, T b));
+
+template<typename T>
+void insertion_sort(T *begin, T *end, int (*cmp)(T a, T b));
+
+template<typename T>
+void quick_sort(T *begin, T *end, int (*cmp)(T a, T b));
+
+template<typename T>
+void merge_sort(T *begin, T *end, int (*cmp)(T a, T b));
+
+//TODO: add heap sort
+
+#endif // CPP_LAB_1_SORT_H
