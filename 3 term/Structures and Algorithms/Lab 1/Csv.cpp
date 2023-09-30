fef2c1e82e1e283419c31b2a28346638a9d08176
Albert Shefner <solaris945@gmail.com>
2023-09-30 19:31:09 +0300

    Added CSV parser

--- 3 term/Structures and Algorithms/Lab 1/Csv.cpp
+++ 3 term/Structures and Algorithms/Lab 1/Csv.cpp
@@ -1,5 +1,8 @@
 #include <fstream>
 #include <cassert>
+#include <functional>
+#include <sstream>
+#include <cstring>
 #include "Csv.h"
 
 Csv::Csv(std::vector<std::string> &headers)  {
@@ -16,7 +19,27 @@ size_t Csv::cols() {
 }
 
 void Csv::append(const std::vector<std::string>& row) {
-    assert(row.size() == cols());
+    if(row.size() != cols()) throw std::exception();
     data->push_back(row);
 }
 
+void Csv::sort(const std::string& field_name, sort_mode mode) {
+    size_t index = 3;
+    auto begin = data->begin().base();
+    auto end = data->end().base();
+
+    std::function<int(std::vector<std::string>&, std::vector<std::string>&)> cmp;
+    switch (mode) {
+        case string:
+            cmp = [index](auto &a, auto &b) {
+               return a[index] < b[index];
+            };
+            break;
+        case number:
+            cmp = [index](auto &a, auto &b) { return (int)(std::stof(a[index]) - std::stof(b[index]));};
+
+    }
+
+    quick_sort(begin, end, cmp);
+    // std::sort(data->begin(), data->end(), cmp);
+}
