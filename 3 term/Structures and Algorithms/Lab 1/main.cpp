4023542d6a956b310d1e579b251a76b7c0717ab5
Albert Shefner <solaris945@gmail.com>
2023-10-01 19:32:50 +0300

    Created benchmark tests for sorting functions.

--- 3 term/Structures and Algorithms/Lab 1/main.cpp
+++ 3 term/Structures and Algorithms/Lab 1/main.cpp
@@ -2,25 +2,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
 #include "sort.h"
 #include "CsvParser.h"
 
 void sort_file();
+
 void test_all_sorts();
 
-template<typename T>
 struct Sort {
+    typedef std::vector<std::string> T;
     std::string name;
     void(*func)(T*, T*, std::function<int(T&, T&)>);
-    Sort(std::string &_name, void(*_func)(T*, T*, std::function<int(T&, T&)>)) {
+    Sort(const char* _name, void(*_func)(T*, T*, std::function<int(T&, T&)>)) {
         name = _name;
         func = _func;
     }
 };
 
+std::vector<Sort> sorts = {
+        Sort("Selection sort", selection_sort),
+        Sort("Bubble sort",bubble_sort),
+        Sort("Insertion sort",insertion_sort),
+        Sort("Merge sort",merge_sort),
+        Sort("Heap sort", heap_sort),
+        Sort("Quick sort", quick_sort),
+};
+
 int main() {
-    auto cmp = *[](int a, int b) { return a - b; };
+    test_all_sorts();
+    return 0;
+}
+
+void sort_file() {
     bool ask_for_path = false;
     std::string input_path = "/home/albert/Downloads/people.csv";
     std::string output_path = "/home/albert/output.csv";
@@ -48,6 +63,37 @@ int main() {
 
     input_file.close();
     output_file.close();
-
-    return 0;
 }
+
+void test_all_sorts() {
+    std::cout << "Testing all sorts.\nEnter a path to CSV file:" << std::endl;
+    std::string path;
+    std::getline(std::cin, path);
+
+    std::cout << "Enter field name:" << std::endl;
+    std::string name;
+    std::getline(std::cin, name);
+
+    std::cout << "Reading file." << std::endl;
+
+    std::ifstream csv_stream(path);
+    CsvParser parser;
+    Csv csv = parser.parse(csv_stream);
+
+    std::vector<Csv> csvs;
+    for(int i = 0; i < sorts.size(); i++) {
+        csvs.emplace_back(csv);
+    }
+
+    for(int i = 0; i < sorts.size(); i++) {
+        std::cout << "Testing: " << sorts[i].name << '\n';
+        auto start = std::chrono::high_resolution_clock::now();
+        csvs[i].sort(name, SortOptions(string, false, sorts[i].func));
+        auto end = std::chrono::high_resolution_clock::now();
+        auto seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+        std::cout << "Sorted for " << seconds << " seconds\n";
+    }
+
+}
+
+
