166b9138dfb6064ad52fb36bafdae93d573b1c6c
Albert Shefner <solaris945@gmail.com>
2023-10-15 15:00:43 +0300

    Added Python folder and Lab 2 in SaA.

---
+++ 3 term/Structures and Algorithms/Lab 2/containers/all.h
@@ -0,0 +1,6 @@
+#ifndef CONTAINERS_ALL_H
+#define CONTAINERS_ALL_H
+
+#include "Queue.h"
+
+#endif // CONTAINERS_ALL_H
---
+++ 3 term/Structures and Algorithms/Lab 2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "containers/all.h"
+
+int main() {
+    auto queue = std::make_unique<Queue<int>>();
+
+    return 0;
+}
