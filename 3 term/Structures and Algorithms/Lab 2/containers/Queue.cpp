e43a3b31fcbc66e77fd78053de4dd0ab3fdc1621
Albert Shefner <solaris945@gmail.com>
2023-10-16 14:46:23 +0300

    Created ArrayList<T>

--- 3 term/Structures and Algorithms/Lab 2/containers/Queue.cpp
+++ 3 term/Structures and Algorithms/Lab 2/containers/Queue.cpp
@@ -6,5 +6,3 @@ template <typename T>
 Queue<T>::Queue() {
     m_arr = std::make_unique<T>(INITIAL_SIZE);
 }
-
-template class Queue<int>;
--- 3 term/Structures and Algorithms/Lab 2/main.cpp
+++ 3 term/Structures and Algorithms/Lab 2/main.cpp
@@ -1,9 +1,38 @@
+#include "containers/ArrayList.h"
 #include <iostream>
 
-#include "containers/all.h"
+// #include "containers/all.h"
 
 int main() {
-    auto queue = std::make_unique<Queue<int>>();
+    auto array_list = std::make_unique<ArrayList<int>>();
 
+    array_list->add(1);
+    array_list->add(2);
+    array_list->add(3);
+    array_list->add(4);
+    array_list->add(5);
+    array_list->add(6);
+    array_list->add(7);
+    array_list->add(8);
+    array_list->add(9);
+    array_list->add(10);
+    array_list->add(11);
+    array_list->add(12);
+    array_list->add(13);
+    array_list->add(14);
+    array_list->add(15);
+    array_list->add(16);
+    array_list->add(17);
+    array_list->add(18);
+
+    std::cout << "Capacity is " << array_list->capacity() << std::endl;
+
+    for (int i = 0; i < array_list->count(); i++) {
+	std::cout << (*array_list)[i] << " ";
+    }
+
+    std::cout << *(array_list->binary_search(10)) << std::endl;
+
+    std::cout << std::endl;
     return 0;
 }
