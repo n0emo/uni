121244221beb71b8406cac744f0cfa103dc72821
Albert Shefner <solaris945@gmail.com>
2023-11-12 14:14:36 +0300

    Created Lab 5

---
+++ 3 term/Programming (Python)/Lab 5/test.py
@@ -0,0 +1,11 @@
+import arithmetic
+
+if __name__ == "__main__":
+    rect1 = arithmetic.Rectangle(5, 4)
+    rect2 = arithmetic.Rectangle(4, 4)
+    print(rect1 == rect2)
+    print(rect1 != rect2)
+    print(rect1 > rect2)
+    print(rect1 >= rect2)
+    print(rect1 < rect2)
+    print(rect1 <= rect2)
