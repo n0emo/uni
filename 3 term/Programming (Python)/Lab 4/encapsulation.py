5593fe1370014c4dae4a8dce7cd9186a0608b0f8
Albert Shefner <solaris945@gmail.com>
2023-11-13 11:38:30 +0300

    Completed Lab 4

--- 3 term/Programming (Python)/Lab 4/encapsulation.py
+++ 3 term/Programming (Python)/Lab 4/encapsulation.py
@@ -3,13 +3,15 @@
 
 __secret__: str = "X452lu"
 
+
 def get_secret() -> str:
     global __secret__
-    return __secret__;
+    return __secret__
+
 
 def set_secret(new: str) -> None:
     if not isinstance(new, str):
-        raise ValueError("New secret must be 'str'");
-    
+        raise ValueError("New secret must be 'str'")
+
     global __secret__
     __secret__ = new
