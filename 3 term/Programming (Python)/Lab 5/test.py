0bef025febc9b147f5028af5f65bb1113b5e252a
Albert Shefner <solaris945@gmail.com>
2023-11-20 02:38:17 +0300

    Updated test.py

--- 3 term/Programming (Python)/Lab 5/test.py
+++ 3 term/Programming (Python)/Lab 5/test.py
@@ -1,12 +1,23 @@
+from unittest import TestCase, main
 import arithmetic
 
+class TestSubsets(TestCase):
+    def test_subsets(self):
+        lst = [2, 4, 10, 1]
+        subsets = arithmetic.SubSets(lst);
+        self.assertCountEqual(
+            sorted(subsets.get_subsets()),
+            sorted([
+                [], [2], [4], [10], [1],
+                [2, 4], [2, 10], [2, 1],
+                [4, 10], [4, 1], [10, 1],
+                [2, 4, 10], [2, 4, 1],
+                [2, 10, 1], [4, 10, 1],
+                [2, 4, 10, 1]
+            ]) 
+        )
+
+
 if __name__ == "__main__":
-    rect1 = arithmetic.Rectangle(5, 4)
-    rect2 = arithmetic.Rectangle(4, 4)
-    print(rect1 == rect2)
-    print(rect1 != rect2)
-    print(rect1 > rect2)
-    print(rect1 >= rect2)
-    print(rect1 < rect2)
-    print(rect1 <= rect2)
+    main()
 
