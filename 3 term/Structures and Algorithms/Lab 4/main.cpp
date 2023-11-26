1d0f06a865f38977460026307ebabbde46fe5476
Albert Shefner <solaris945@gmail.com>
2023-11-27 01:35:29 +0300

    Implemented Levenshtein Distance

--- 3 term/Structures and Algorithms/Lab 4/main.cpp
+++ 3 term/Structures and Algorithms/Lab 4/main.cpp
@@ -1,15 +1,23 @@
+#include <array>
 #include <cstddef>
 #include <iostream>
+
 #include "strs.h"
 
 int main() {
-    std::cout << "Hello, World!\n";
-    const char* a = "kitten";
-    size_t a_size = 6;
-    const char* b = "sitting";
-    size_t b_size = 7;
+    std::array<std::string, 5> as = {"book", "apple", "kitten", "lol",
+                                     "smal str"};
+    std::array<std::string, 5> bs = {"back", "appel", "sitting", "xd",
+                                     "big string"};
 
-    int result = l_distance_naive(a, a_size, b, b_size);
-    std::cout << result << std::endl;
+    for (size_t i = 0; i < as.size(); i++) {
+        auto result_naive = l_distance_naive(as[i], bs[i]);
+        auto result_optimal = l_distance(as[i], bs[i]);
+
+        std::cout << "Strings: \"" << as[i] << "\", \"" << bs[i] << "\"\n";
+        std::cout << "Results: naive = " << result_naive
+                  << ", optimal = " << result_optimal << std::endl;
+    }
+
     return 0;
 }
