76ca40afee9fc60fcd963a8eb8455057c6550008
Albert Shefner <solaris945@gmail.com>
2023-11-27 11:14:45 +0300

    Added prefix_f and substr_index_rk

--- 3 term/Structures and Algorithms/Lab 4/main.cpp
+++ 3 term/Structures and Algorithms/Lab 4/main.cpp
@@ -1,23 +1,33 @@
 #include <array>
 #include <cstddef>
+#include <functional>
 #include <iostream>
 
 #include "strs.h"
 
 int main() {
-    std::array<std::string, 5> as = {"book", "apple", "kitten", "lol",
-                                     "smal str"};
-    std::array<std::string, 5> bs = {"back", "appel", "sitting", "xd",
-                                     "big string"};
+    // std::array<std::string, 5> as = {"book", "apple", "kitten", "lol",
+    //                                  "smal str"};
+    // std::array<std::string, 5> bs = {"back", "appel", "sitting", "xd",
+    //                                  "big string"};
+    //
+    // for (size_t i = 0; i < as.size(); i++) {
+    //     auto result_naive = l_distance_naive(as[i], bs[i]);
+    //     auto result_optimal = l_distance(as[i], bs[i]);
+    //
+    //     std::cout << "Strings: \"" << as[i] << "\", \"" << bs[i] << "\"\n";
+    //     std::cout << "Results: naive = " << result_naive
+    //               << ", optimal = " << result_optimal << std::endl;
+    // }
+    // substr_index_rk<std::hash<std::string>>("xdxdaaxdaabmwxdadxd", "bmw");
+    // std::cout << substr_index_rk("xdxdaaxdaabmwxdadxd", "bmw").value()
+    //           << std::endl;
 
-    for (size_t i = 0; i < as.size(); i++) {
-        auto result_naive = l_distance_naive(as[i], bs[i]);
-        auto result_optimal = l_distance(as[i], bs[i]);
-
-        std::cout << "Strings: \"" << as[i] << "\", \"" << bs[i] << "\"\n";
-        std::cout << "Results: naive = " << result_naive
-                  << ", optimal = " << result_optimal << std::endl;
+    std::string s = "abcdabcabcdabcdab";
+    auto result = prefix_f(s);
+    for (auto n : result) {
+        std::cout << n << " ";
     }
-
+    std::cout << std::endl;
     return 0;
 }
