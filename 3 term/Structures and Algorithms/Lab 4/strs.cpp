68f24ea84de8621ae00a4ad0375b9e99483ceedb
Albert Shefner <solaris945@gmail.com>
2023-11-27 11:14:45 +0300

    Added prefix_f and substr_index_rk

--- 3 term/Structures and Algorithms/Lab 4/strs.cpp
+++ 3 term/Structures and Algorithms/Lab 4/strs.cpp
@@ -1,7 +1,18 @@
 #include "strs.h"
 
 #include <algorithm>
-#include <cstddef>
+#include <functional>
+#include <iostream>
+
+template <typename THash = PolynomialHash>
+size_t str_hash(const std::string& str, size_t start = 0,
+                size_t count = SIZE_MAX) {
+    if (count == SIZE_MAX) {
+        count = str.size();
+    }
+
+    return THash{}(str.substr(start, count));
+}
 
 template <typename T>
 T min3(T v1, T v2, T v3) {
@@ -9,6 +20,48 @@ T min3(T v1, T v2, T v3) {
 }
 
 std::string tail(std::string x) { return x.substr(0, x.size() - 1); }
+
+template <typename THash>
+std::optional<size_t> substr_index_rk(const std::string& str,
+                                      const std::string& substr) {
+    size_t substr_size = substr.size();
+    size_t str_size = str.size();
+    if (substr_size > str_size) {
+        return std::nullopt;
+    }
+
+    size_t substr_hash = str_hash<THash>(substr);
+    std::cout << substr_hash << std::endl;
+
+    for (size_t i = 0; i < str_size - substr_size; i++) {
+        size_t hash = str_hash<THash>(str, i, substr_size);
+        std::cout << hash << " ";
+        if (hash == substr_hash && str.substr(i, substr_size) == substr) {
+            std::cout << std::endl;
+            return std::optional(i);
+        }
+    }
+
+    return std::nullopt;
+}
+
+std::vector<size_t> prefix_f(const std::string& s) {
+    size_t size = s.size();
+    std::vector<size_t> result(size);
+
+    for (size_t i = 1; i < size; i++) {
+        size_t max_prefix = result[i - 1];
+        while (max_prefix > 0 && s[max_prefix] != s[i]) {
+            max_prefix = result[max_prefix - 1];
+        }
+        if (s[max_prefix] == s[i]) {
+            max_prefix += 1;
+        }
+        result[i] = max_prefix;
+    }
+
+    return result;
+}
 
 size_t l_distance_naive(std::string a, std::string b) {
     if (a.size() == 0) {
@@ -31,7 +84,6 @@ size_t l_distance(std::string a, std::string b) {
     for (size_t i = 0; i <= b.size(); i++) {
         v0[i] = i;
     }
-
     for (size_t i = 0; i < a.size(); i++) {
         v1[0] = i + 1;
         for (size_t j = 0; j < b.size(); j++) {
@@ -48,3 +100,9 @@ size_t l_distance(std::string a, std::string b) {
     delete[] v1;
     return v0[b.size()];
 }
+
+template std::optional<size_t> substr_index_rk<PolynomialHash>(
+    const std::string& str, const std::string& substr);
+
+template std::optional<size_t> substr_index_rk<std::hash<std::string>>(
+    const std::string& str, const std::string& substr);
--- 3 term/Structures and Algorithms/Lab 4/strs.h
+++ 3 term/Structures and Algorithms/Lab 4/strs.h
@@ -2,7 +2,32 @@
 #define STRS_H
 
 #include <cstddef>
+#include <cstdint>
+#include <optional>
 #include <string>
+#include <vector>
+
+struct PolynomialHash {
+    size_t window = INT32_MAX;
+    size_t prime = 31;
+
+    size_t operator()(const std::string& s) const {
+        size_t hash = 0;
+        size_t pow = 1;
+        for (size_t i = 0; i < s.size(); i++) {
+            hash = ((hash + s[i]) * pow) % window;
+            pow = (pow * prime) % window;
+        }
+
+        return hash;
+    }
+};
+
+template <typename THash = PolynomialHash>
+std::optional<size_t> substr_index_rk(const std::string& str,
+                                      const std::string& substr);
+
+std::vector<size_t> prefix_f(const std::string& s);
 
 size_t l_distance_naive(std::string a, std::string b);
 
