c4b5960c2ea8d7739648ffa1a1c3b249397c78b1
Albert Shefner <solaris945@gmail.com>
2023-12-04 01:30:27 +0300

    Completed Lab 3

--- 3 term/Structures and Algorithms/Lab 3/interface/HHashTable.cpp
+++ 3 term/Structures and Algorithms/Lab 3/interface/HHashTable.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <unordered_map>
 
 #include "../include/HashTable.hpp"
 
@@ -45,4 +46,65 @@ void ht_s_count_collisions_(void** ht, int* result) {
     auto table =
         reinterpret_cast<HashTable<string, string, hash<string>>*>(*ht);
     *result = table->count_collisions();
+}
+
+void ht_p_create_(void** ht) {
+    using namespace std;
+    try {
+        *ht = reinterpret_cast<void*>(
+            new HashTable<string, string, PolynomialHash>());
+    } catch (exception) {
+        *ht = NULL;
+    }
+}
+
+void ht_p_destroy_(void** ht) {
+    using namespace std;
+    auto ptr_to_del =
+        reinterpret_cast<HashTable<string, string, PolynomialHash>**>(ht);
+    delete *ptr_to_del;
+}
+
+void ht_p_add_(void** ht, const char* key, const char* value) {
+    using namespace std;
+    auto table =
+        reinterpret_cast<HashTable<string, string, PolynomialHash>*>(*ht);
+    table->add(key, value);
+}
+
+void ht_p_get_(void** ht, const char* key, const char* result) {
+    using namespace std;
+    auto table =
+        reinterpret_cast<HashTable<string, string, PolynomialHash>*>(*ht);
+    auto result_str = table->get(key).value_or("NULL");
+    std::cout << result_str << std::endl;
+    result = result_str.c_str();
+}
+
+void ht_p_count_collisions_(void** ht, int* result) {
+    using namespace std;
+    auto table =
+        reinterpret_cast<HashTable<string, string, PolynomialHash>*>(*ht);
+    *result = table->count_collisions();
+}
+
+void map_create_(void** map) {
+    using namespace std;
+    try {
+        *map = reinterpret_cast<void*>(new unordered_map<string, string>());
+    } catch (exception) {
+        *map = NULL;
+    }
+}
+
+void map_destroy_(void** map) {
+    using namespace std;
+    auto mp = reinterpret_cast<unordered_map<string, string>*>(*map);
+    delete mp;
+}
+
+void map_add_(void** map, const char* key, const char* value) {
+    using namespace std;
+    auto mp = reinterpret_cast<unordered_map<string, string>*>(*map);
+    mp->insert(pair(key, value));
 }
--- 3 term/Structures and Algorithms/Lab 3/prog.cpp
+++ 3 term/Structures and Algorithms/Lab 3/prog.cpp
@@ -4,51 +4,6 @@
 
 #include "include/HashTable.hpp"
 
-struct PolynomialHash {
-    struct Powers {
-        static const size_t INITIAL_SIZE = 1;
-        static const size_t PRIME = 31;
-
-        Powers() {
-            _vec[0] = 1;
-            for (size_t i = 1; i < INITIAL_SIZE; i++) {
-                _vec[i] = _vec[i - 1] * PRIME;
-            }
-        }
-
-        size_t operator[](size_t index) {
-            if (index >= _size) {
-                expand();
-            }
-            return _vec[index];
-        }
-
-    private:
-        std::vector<size_t> _vec = std::vector<size_t>(INITIAL_SIZE);
-        size_t _size = INITIAL_SIZE;
-
-        void expand() {
-            size_t new_size = _size * 2;
-            _vec.resize(new_size);
-            for (size_t i = 0; i < new_size - _size; i++) {
-                _vec[_size + i] = _vec[_size + i - 1] * PRIME;
-            }
-            _size = new_size;
-        }
-    };
-
-    inline static Powers powers = Powers();
-
-    size_t operator()(const std::string& s) const {
-        size_t hash = 0;
-        for (size_t i = 0; i < s.size(); i++) {
-            hash = (hash + s[i]) * powers[i];
-        }
-        // std::cout << "Hash for \"" << s << "\": " << hash << std::endl;
-        return hash;
-    }
-};
-
 int main() {
     auto table = std::make_unique<HashTable<std::string, std::string>>();
     return 0;
