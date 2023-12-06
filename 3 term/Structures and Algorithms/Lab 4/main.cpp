582004549cefb96e289f9525f300bf2ed4357cdb
Albert Shefner <solaris945@gmail.com>
2023-12-06 12:11:07 +0300

    Added unicode support

--- 3 term/Structures and Algorithms/Lab 4/main.cpp
+++ 3 term/Structures and Algorithms/Lab 4/main.cpp
@@ -1,11 +1,17 @@
 #include <array>
+#include <codecvt>
 #include <cstddef>
 #include <functional>
 #include <iostream>
+#include <locale>
 
 #include "strs.h"
 
 int main() {
+    std::wbuffer_convert<std::codecvt_utf8<wchar_t>> conv_out(
+        std::cout.rdbuf());
+    std::wostream out(&conv_out);
+
     // std::array<std::string, 5> as = {"book", "apple", "kitten", "lol",
     //                                  "smal str"};
     // std::array<std::string, 5> bs = {"back", "appel", "sitting", "xd",
@@ -15,37 +21,38 @@ int main() {
     //     auto result_naive = l_distance_naive(as[i], bs[i]);
     //     auto result_optimal = l_distance(as[i], bs[i]);
     //
-    //     std::cout << "Strings: \"" << as[i] << "\", \"" << bs[i] << "\"\n";
-    //     std::cout << "Results: naive = " << result_naive
+    //     out << "Strings: \"" << as[i] << "\", \"" << bs[i] << "\"\n";
+    //     out << "Results: naive = " << result_naive
     //               << ", optimal = " << result_optimal << std::endl;
     // }
-    // substr_index_rk<std::hash<std::string>>("xdxdaaxdaabmwxdadxd", "bmw");
-    // std::cout << substr_index_rk("xdxdaaxdaabmwxdadxd", "bmw").value()
-    //           << std::endl;
-
-    // std::string s = "abcdabcabcdabcdab";
+    // substr_index_rk<std::hash<std::wstring>>(L"xdxdaaxdaabmwxdadxd", L"bmw");
+    // out << substr_index_rk(L"xdxdaaxdaabmwxdadxd", L"bmw").value() <<
+    // std::endl;
+    //
+    // std::wstring s = L"abcdabcabcdabcdab";
     // auto result = prefix_f(s);
     // for (auto n : result) {
-    //     std::cout << n << " ";
+    //     out << n << " ";
     // }
-    // std::cout << std::endl;
+    // out << std::endl;
 
     // std::string s = "aaaabmwaabmwaaaabmw";
     // auto result = substr_index_kmp(s, "bmw");
     // for (auto n : result) {
-    //     std::cout << n << " ";
+    //     out << n << " ";
     // }
-    // std::cout << std::endl;
+    // out << std::endl;
 
-    // std::string a = "abcdef";
-    // std::string b = "oobcdx";
-    // std::cout << lcs(a, b) << std::endl;
-    // std::cout << lcs(a, b).size() << std::endl;
-    std::wstring a = L"гипнотизёр";
-    std::wstring b = L"гипноз";
+    std::wstring a = L"абвгд";
+    std::wstring b = L"ооовг";
+    out << lcs(a, b) << std::endl;
+    out << lcs(a, b).size() << std::endl;
 
-    std::cout << l_distance(a, b) << std::endl;
-    // std::cout << l_distance_naive(a, b) << std::endl;
+    // out << L"日本国" << std::endl;
+    // std::wstring a = L"гипнотизёр";
+    // std::wstring b = L"гипноз";
+    // out << l_distance(a, b) << std::endl;
+    // out << l_distance_naive(a, b) << std::endl;
 
     return 0;
 }
