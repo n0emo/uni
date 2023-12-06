6bc2b124104118ac68591f76d2e9523404427f03
Albert Shefner <solaris945@gmail.com>
2023-12-06 12:11:07 +0300

    Added unicode support

--- 3 term/Structures and Algorithms/Lab 4/strs.h
+++ 3 term/Structures and Algorithms/Lab 4/strs.h
@@ -8,33 +8,36 @@
 #include <vector>
 
 struct PolynomialHash {
-    size_t window = INT32_MAX;
-    size_t prime = 31;
+    struct Powers {
+        static const size_t INITIAL_SIZE = 1;
+        static const size_t PRIME = 31;
 
-    size_t operator()(const std::string& s) const {
-        size_t hash = 0;
-        size_t pow = 1;
-        for (size_t i = 0; i < s.size(); i++) {
-            hash = ((hash + s[i]) * pow) % window;
-            pow = (pow * prime) % window;
-        }
+        Powers();
+        size_t operator[](size_t index);
 
-        return hash;
-    }
+    private:
+        std::vector<size_t> _vec = std::vector<size_t>(INITIAL_SIZE);
+        size_t _size = INITIAL_SIZE;
+
+        void expand();
+    };
+
+    inline static Powers powers = Powers();
+    size_t operator()(const std::wstring& s) const;
 };
 
 template <typename THash = PolynomialHash>
-std::optional<size_t> substr_index_rk(const std::string& str,
-                                      const std::string& substr);
+std::optional<size_t> substr_index_rk(const std::wstring& str,
+                                      const std::wstring& substr);
 
-std::vector<size_t> prefix_f(const std::string& s);
+std::vector<size_t> prefix_f(const std::wstring& s);
 
-std::vector<size_t> substr_index_kmp(const std::string& str,
-                                     const std::string& substr);
+std::vector<size_t> substr_index_kmp(const std::wstring& str,
+                                     const std::wstring& substr);
 
-std::string lcs(const std::string& a, const std::string& b);
+std::wstring lcs(const std::wstring& a, const std::wstring& b);
 
-size_t l_distance_naive(std::string a, std::string b);
+size_t l_distance_naive(std::wstring a, std::wstring b);
 
 size_t l_distance(const std::wstring& a, const std::wstring& b);
 
