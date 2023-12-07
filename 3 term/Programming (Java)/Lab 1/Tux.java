99c7f830009b87aeec6a2d626c23f1b3929ae464
Albert Shefner <solaris945@gmail.com>
2023-12-07 03:11:10 +0300

    Completed lab 1

--- 3 term/Programming (Java)/Lab 1/Tux.java
+++ 3 term/Programming (Java)/Lab 1/Tux.java
@@ -1,10 +1,13 @@
 import java.awt.*;
 
 public class Tux {
-    private static final Dimension _windowSize = new Dimension(800, 800);
+    private static final Dimension _windowSize = new Dimension(1000, 1000);
 
     public static void main(String[] args) {
-        Canvas canvas = new TuxCanvas(_windowSize);
+        boolean printAuthor = !arrayContains(args, "-n");
+        boolean drawReference = arrayContains(args, "-r");
+
+        Canvas canvas = new TuxCanvas(printAuthor, drawReference);
 
         Frame frame = new Frame();
         frame.add(canvas);
@@ -12,5 +15,14 @@ public class Tux {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.addWindowListener(new TuxWindowAdapter());
+    }
+
+    private static <T> boolean arrayContains(T[] array, T elem) {
+        for (T e : array) {
+            if (elem.equals(e)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
