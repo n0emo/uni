9a812ebde7bd48733bcb2b7b72eb8bf4fc291e46
Albert Shefner <solaris945@gmail.com>
2023-12-09 18:56:07 +0300

    Completed code for lab 7

--- 3 term/Programming (Python)/Lab 7/task04.py
+++ 3 term/Programming (Python)/Lab 7/task04.py
@@ -1,23 +1,54 @@
 from collections import namedtuple
 from multiprocessing import Process, Queue
-from typing import Any, Callable, Iterable, Mapping
 
-import numpy as np
+import array
+from typing import List
+Result = namedtuple("Result", "thread row")
 
-Result = namedtuple("result", "thread row")
 
-
-class RowProcess(Process):
-    def __init__(
-        self,
+def mul_row(
         queue: Queue,
-        group: None = None,
-        target: Callable[..., object] | None = None,
-        name: str | None = None,
-        args: Iterable[Any] = [],
-        kwargs: Mapping[str, Any] = {},
-        *,
-        daemon: bool | None = None
+        arr_a: List[array.array],
+        arr_b: List[array.array],
+        row: int
     ) -> None:
-        super().__init__(group, target, name, args, kwargs, daemon=daemon)
-        self.queue = queue
+    result = array.array("d")
+    for col in range(len(arr_a)):
+        s = 0
+        for i in range(len(arr_b)):
+            s += arr_a[row][i] * arr_b[i][col]
+        result.append(s)
+
+    queue.put(Result(row, result))
+
+def mat_mul(a, b):
+    queue = Queue()
+    processes = []
+
+    for i in range(len(a)):
+        process = Process(target=mul_row, args=(queue, a, b, i))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    results = []
+    while not queue.empty():
+        results.append(queue.get())
+
+    results.sort(key=lambda r : r.row)
+    return list(map(lambda r: r.row, results))
+
+mat_a = [
+    array.array("d", [1, 2]),
+    array.array("d", [3, 4]),
+    array.array("d", [5, 6]),
+]
+
+mat_b = [
+    array.array("d", [7, 8, 9]),
+    array.array("d", [10, 11, 12]),
+]
+
+print(mat_mul(mat_a, mat_b))
