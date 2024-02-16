1e59a31026c36bd98c239999bd7947531901d8af
Albert Shefner <solaris945@gmail.com>
2024-02-16 23:36:36 +0300

    Migrate to abuild

---
+++ 1 term/Entrance to C/Lab 4/src/abuild.cpp
@@ -0,0 +1,36 @@
+#define ABUILD_IMPLEMENTATION
+#include "../../../../abuild/abuild.hpp"
+
+using namespace ab;
+
+int main(int argc, char **argv)
+{
+    self_update(argc, argv, __FILE__);
+    make_directory_if_not_exists("build");
+
+    c::CompileOptions options
+    {
+        .compiler = c::GCC, 
+        .flags = {"-Wall", "-Wextra", "-pedantic"},
+        .libs = {"m"}
+    };
+
+    std::string subcommand = "";
+    if (argc > 1)
+        subcommand = argv[1];
+
+    if (subcommand == "" || subcommand == "1")
+    {
+        log::info("Building task 1");
+        options.exe_cmd(path({"build", "task_1"}), {"task_1.c"})
+            .run_or_die("Error during compilation");
+    }
+    if (subcommand == "" || subcommand == "2")
+    {
+        log::info("Building task 2");
+        options.exe_cmd(path({"build", "task_2"}), {"task_2.c"})
+            .run_or_die("Error during compilation");
+    }
+
+    return 0;
+}
--- 1 term/Entrance to C/Lab 7/src/main.c
+++ 1 term/Entrance to C/Lab 7/src/main.c
@@ -7,7 +7,8 @@
 // Если хотите ввести их с консоли - 0.
 #define random_matrix 0
 
-struct point {
+struct point
+{
     int row;
     int col;
 };
@@ -16,13 +17,14 @@ void get_matrix(int ***matrix, int rows, int cols);
 void find_elements(int **matrix, int rows, int cols, int *count_ptr, struct point *vector);
 void free_matrix(int **matrix, int rows);
 
-int main() {
+int main()
+{
     int rows, cols, **matrix_S;
 
-    printf_s("Enter rows and columns count:\n");
-    scanf_s("%i %i", &rows, &cols);
+    printf("Enter rows and columns count:\n");
+    scanf("%i %i", &rows, &cols);
 
-    printf_s("Enter matrix with %i rows and %i columns:\n", rows, cols);
+    printf("Enter matrix with %i rows and %i columns:\n", rows, cols);
     get_matrix(&matrix_S, rows, cols);
 
     struct point vector_P[rows * cols];
@@ -30,10 +32,11 @@ int main() {
 
     find_elements(matrix_S, rows, cols, &count, vector_P);
 
-    printf_s("\nresult:\n");
-    for (int i = 0; i < count; i++) {
-        printf_s("matrix_S[%d][%d], value=%d\n", vector_P[i].row, vector_P[i].col,
-                 matrix_S[vector_P[i].row][vector_P[i].col]);
+    printf("\nresult:\n");
+    for (int i = 0; i < count; i++)
+    {
+        printf("matrix_S[%d][%d], value=%d\n", vector_P[i].row, vector_P[i].col,
+               matrix_S[vector_P[i].row][vector_P[i].col]);
     }
 
     free_matrix(matrix_S, rows);
@@ -41,34 +44,45 @@ int main() {
     return 0;
 }
 
-void get_matrix(int ***matrix, int rows, int cols) {
+void get_matrix(int ***matrix, int rows, int cols)
+{
     srand(time(NULL));
 
-    *matrix = (int **) malloc(rows * sizeof(int *));
+    *matrix = (int **)malloc(rows * sizeof(int *));
 
-    for (int i = 0; i < rows; i++) {
-        (*matrix)[i] = (int *) malloc(cols * sizeof(int));
+    for (int i = 0; i < rows; i++)
+    {
+        (*matrix)[i] = (int *)malloc(cols * sizeof(int));
 
-        for (int j = 0; j < cols; j++) {
-            if (random_matrix) {
+        for (int j = 0; j < cols; j++)
+        {
+            if (random_matrix)
+            {
                 (*matrix)[i][j] = rand() % 101 - 50;
                 printf("%3i ", (*matrix)[i][j]);
-            } else {
-                scanf_s("%i", &(*matrix)[i][j]);
+            }
+            else
+            {
+                scanf("%i", &(*matrix)[i][j]);
             }
         }
-        if (random_matrix) {
-            printf_s("\n");
+        if (random_matrix)
+        {
+            printf("\n");
         }
     }
 }
 
-void find_elements(int **matrix, int rows, int cols, int *count_ptr, struct point *vector) {
+void find_elements(int **matrix, int rows, int cols, int *count_ptr, struct point *vector)
+{
     struct point tmp;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = i + 1; j < cols; j++) {
-            if (matrix[i][j] > 0) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i + 1; j < cols; j++)
+        {
+            if (matrix[i][j] > 0)
+            {
                 tmp.row = i;
                 tmp.col = j;
                 vector[*count_ptr] = tmp;
@@ -78,8 +92,10 @@ void find_elements(int **matrix, int rows, int cols, int *count_ptr, struct poin
     }
 }
 
-void free_matrix(int **matrix, int rows) {
-    for (int i = 0; i < rows; i++) {
+void free_matrix(int **matrix, int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
         free(matrix[i]);
     }
     free(matrix);
