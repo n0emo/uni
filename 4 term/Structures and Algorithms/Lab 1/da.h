96580dbb05924a2e67e7dd59439f5c41c8081cd6
Albert Shefner <solaris945@gmail.com>
2024-02-18 16:54:00 +0300

    Create lab 1

---
+++ 4 term/Structures and Algorithms/Lab 1/da.h
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#define INITIAL_CAP 16
+
+#define da_append(da, item)                                                                                            \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        if ((da).count == (da).capacity)                                                                               \
+        {                                                                                                              \
+            size_t new_cap = (da).capacity ? (da).capacity * 2 : INITIAL_CAP;                                          \
+            (da).items = realloc((da).items, sizeof((da).items[0]) * new_cap);                                         \
+            assert((da).items != NULL && "Buy more RAM lol");                                                          \
+            (da).capacity = new_cap;                                                                                   \
+        }                                                                                                              \
+        (da).items[(da).count] = item;                                                                                 \
+        (da).count++;                                                                                                  \
+    } while (0)
