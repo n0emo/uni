024b8d7436a03771b3dd9398c88c4ba3df95580a
Albert Shefner <solaris945@gmail.com>
2024-03-05 17:38:14 +0300

    Abstract knapsack from solution algorithm

---
+++ 4 term/Structures and Algorithms/Lab 1/include/solver/common.hpp
@@ -0,0 +1,42 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <vector>
+
+namespace solver
+{
+typedef std::vector<char> Bits;
+
+template <typename Item>
+using predicate = std::function<bool(const std::vector<Item> &)>;
+
+template <typename Item>
+using compare = std::function<bool(const std::vector<Item> &, const std::vector<Item> &)>;
+
+template <typename Item>
+using get_chosen_t = std::function<std::vector<Item>(Bits bits)>;
+
+template <typename Item>
+using eval_t = std::function<std::vector<Item>(const std::vector<Item> &)>;
+
+template <typename Item>
+constexpr get_chosen_t<Item> make_get_chosen(const std::vector<Item> &items)
+{
+    return [&](Bits bits) constexpr {
+        std::vector<Item> result;
+        for (size_t i = 0; i < bits.size(); i++)
+        {
+            if (bits[i])
+                result.emplace_back(items[i]);
+        }
+
+        return result;
+    };
+}
+} // namespace solver
+
+#endif // COMMON_HPP
