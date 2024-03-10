e085ac65de1538a6a80f5faae634a2e7cad219fe
Albert Shefner <solaris945@gmail.com>
2024-03-10 19:04:43 +0300

    Initial normalization

--- 4 term/Structures and Algorithms/Lab 1/include/sat/tree.hpp
+++ 4 term/Structures and Algorithms/Lab 1/include/sat/tree.hpp
@@ -7,26 +7,43 @@
 #include <variant>
 #include <vector>
 
-struct NodeLeaf;
+struct Tree;
+
+struct Var;
+typedef bool Const;
+
+typedef std::variant<Var, Const> NodeLeaf;
 struct NodeUnOp;
 struct NodeBinOp;
-typedef std::variant<NodeLeaf, NodeUnOp, NodeBinOp> Node;
+struct NodeOp;
+typedef std::variant<NodeLeaf, NodeUnOp, NodeBinOp, NodeOp> Node;
 
-struct NodeLeaf
+void print_indent(size_t depth);
+std::optional<size_t> find_root(const std::vector<Token> &tokens);
+std::unique_ptr<Node> build_tree_node(const std::vector<Token> &tokens);
+void normilize_node(std::unique_ptr<Node> &node);
+
+struct Tree
 {
-    Token tok;
-    NodeLeaf(Token tok) : tok(tok)
-    {
-    }
+    std::unique_ptr<Node> root;
+
+    void print(const std::unique_ptr<Node> &root = nullptr, size_t depth = 0) const;
+    void normalize();
+    void simplify();
+
+    static Tree build_tree(const std::vector<Token> &tokens);
+};
+
+struct Var
+{
+    int num;
+    bool inverse;
 };
 
 struct NodeUnOp
 {
     Token operation;
     std::unique_ptr<Node> operand;
-    NodeUnOp(Token operation, std::unique_ptr<Node> operand) : operation(operation), operand(std::move(operand))
-    {
-    }
 };
 
 struct NodeBinOp
@@ -34,18 +51,22 @@ struct NodeBinOp
     Token operation;
     std::unique_ptr<Node> left_operand;
     std::unique_ptr<Node> right_operand;
-    NodeBinOp(Token operation, std::unique_ptr<Node> left_operand, std::unique_ptr<Node> right_operand)
-        : operation(operation), left_operand(std::move(left_operand)), right_operand(std::move(right_operand))
-    {
-    }
 };
 
-struct Tree
+struct NodeOp
 {
-    std::unique_ptr<Node> root;
+    Token operation;
+    std::vector<std::unique_ptr<Node>> operands;
 
-    static std::unique_ptr<Node> build_tree(const std::vector<Token> &tokens);
-    static std::optional<size_t> find_root(const std::vector<Token> &tokens);
+    NodeOp(Token operation, std::vector<std::unique_ptr<Node>> &operands)
+        : operation(operation)
+    {
+        this->operands = std::vector<std::unique_ptr<Node>>();
+        for (auto &op : operands)
+        {
+            this->operands.emplace_back(std::move(op));
+        }
+    }
 };
 
 #endif // TREE_HPP
