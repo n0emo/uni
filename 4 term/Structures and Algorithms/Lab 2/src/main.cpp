9f1d901c783edb21724ea2910599422967b11229
Albert Shefner <solaris945@gmail.com>
2024-04-08 11:38:19 +0300

    Implenebt subgraphs

--- 4 term/Structures and Algorithms/Lab 2/src/main.cpp
+++ 4 term/Structures and Algorithms/Lab 2/src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <exception>
 #include <iostream>
 #include <map>
@@ -88,42 +89,102 @@ std::vector<T> dfs(const Graph<T> &graph, const T& root)
 	return search<std::stack<T>, T>(graph, root);
 }
 
-// TODO
 template <typename T>
-std::vector<std::vector<T>> forest(const Graph<T> &graph)
+std::vector<std::vector<T>> subgraphs(const Graph<T> &graph)
 {
 	std::map<T, bool> visited;
+	std::vector<std::vector<T>> result;
 
 	for(const auto &v : graph)
 	{
 		visited.insert({v.first,  false});
 	}
+
+	while(!visited.empty())
+	{
+		std::vector<T> result_current;
+
+		std::stack<T> stack;
+		stack.push((*visited.begin()).first);
+
+		while(!stack.empty())
+		{
+			T current = top(stack);
+			stack.pop();
+			if(visited[current]) continue;
+			visited[current] = true;
+
+			result_current.emplace_back(current);
+			auto edges_search = graph.find(current);
+			if(edges_search == graph.end())
+			{
+				std::terminate();
+			}
+
+			for(const auto &edge : edges_search->second)
+			{
+				stack.push(edge);
+			}
+		}
+		result.emplace_back(std::move(result_current));
+
+		for(auto it = visited.begin(); it != visited.end();)
+		{
+			if(it->second)
+			{
+				it = visited.erase(it);
+			}
+			else 
+			{
+				++it;
+			}
+		}
+	}
+	return result;
+}
+
+template <typename T>
+void print_vec(const std::vector<T> &vec, std::string_view delim = ", ")
+{
+	if(vec.empty()) return;
+	auto it = vec.begin();
+	std::cout << *it;
+	for(++it; it != vec.end(); ++it)
+	{
+		std::cout << delim << *it;
+	}
+	std::cout << '\n';
 }
 
 int main()
 {
 	auto graph = std::make_unique<Graph<int>>(Graph<int>{
 		{1, {1, 2, 3}},
-		{2, {5, 2, 3, 6}},
-		{3, {4, 2}},
-		{4, {1, 5}},
-		{5, {2}},
-		{6, {}}
+		{2, {1, 2, 3}},
+		{3, {1, 2}},
+		{4, {6, 5}},
+		{5, {4}},
+		{6, {}},
+		{7, {}}
+		
 	});
 
+	std::cout << "Graph:\n";
 	graph_print(*graph);
+	std::cout << '\n';
 
-	std::cout << "DFS:";
-	for (const auto& v : dfs<int>(*graph, 1))
-	{
-		std::cout << " " << v;
-	}
-	std::cout << std::endl;
+	std::cout << "DFS: ";
+	print_vec(dfs<int>(*graph, 1));
+	std::cout << '\n';
 
-	std::cout << "BFS:";
-	for (const auto& v : bfs<int>(*graph, 1))
+	std::cout << "BFS: ";
+	print_vec(bfs<int>(*graph, 1));
+	std::cout << '\n';
+
+	std::cout << "Subgraphs:\n";
+	for (const auto &vec : subgraphs<int>(*graph))
 	{
-		std::cout << " " << v;
+		print_vec(vec);
 	}
 	std::cout << std::endl;
 
