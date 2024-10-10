e58c36cf3d95bf0d9a395f996d6696127a89106b
Albert Shefner <dev_n0emo@tuta.io>
2024-10-10 13:07:52 +0300

    5/Java/1 Handle exception

--- Term5/Java/lab-1-sort/src/sort/core.clj
+++ Term5/Java/lab-1-sort/src/sort/core.clj
@@ -5,17 +5,21 @@
 (defn quick-sort
   ([elements] (quick-sort > elements))
   ([cmp [pivot & elements]]
-  (let [greater? #(cmp % pivot)]
-   (when pivot
-    (lazy-cat
-      (quick-sort cmp (remove greater? elements))
-      [pivot]
-      (quick-sort cmp (filter greater? elements)))))))
+   (let [greater? #(cmp % pivot)]
+     (when pivot
+       (lazy-cat
+        (quick-sort cmp (remove greater? elements))
+        [pivot]
+        (quick-sort cmp (filter greater? elements)))))))
 
 (defn -main
   [& args]
-  (->> args
-    (map #(. Float parseFloat %))
-    (quick-sort)
-    (str/join " ")
-    (println)))
+  (try (->> args
+            (map #(. Float parseFloat %))
+            (quick-sort)
+            (str/join " ")
+            (println))
+       (catch Exception e
+         (do
+           (println "Error:" (.toString e))
+           (System/exit 1)))))
