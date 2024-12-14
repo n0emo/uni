f290ce3e04d77a945e808d1186b441baa7ba48b3
Albert Shefner <dev_n0emo@tuta.io>
2024-12-14 14:42:18 +0300

    5/Java/3 finalize code

--- Term5/Java/lab-3-reflect/src/reflections/core.clj
+++ Term5/Java/lab-3-reflect/src/reflections/core.clj
@@ -1,19 +1,65 @@
 (ns reflections.core
   (:gen-class)
-  (:require [clojure.reflect :as cr]
-            [clojure.pprint :as pprint])
-  (:import [org.reflections Reflections]
-           [org.reflections ConfigurationBuilder]))
+  (:require
+   [clojure.reflect :as cr]
+   [clojure.string :as str]))
 
-(defn get-classes [package-name]
-  (let [config (-> (ConfigurationBuilder.)
-                   (.setUrls (java.util.Collections/singleton (java.net.URL. (str "file:./target/classes/"))))
-                   (.setScanners (into-array org.reflections.scanners.Scanner [org.reflections.scanners.SubTypesScanner.])))]
-    (let [reflect (Reflections. config)]
-      ; Here you can return the classes or do something with them
-      (.getSubTypesOf reflect Object))) ; Example: get all subclasses of Object
-  )
+(defn- print-sorted [fields]
+  (if (empty? fields)
+    (println "  none")
+    (doseq [f (sort fields)]
+      (println " " f))))
+
+(defn- print-members [members msg to-str predicate]
+  (println msg)
+  (print-sorted (-> [f (filter predicate members)]
+                    (for (to-str f)))))
+
+(defn- static? [member]
+  (contains? (get member :flags) :static))
+
+(defn- field? [m]
+  (contains? m :type))
+
+(defn- method? [m]
+  (contains? m :return-type))
+
+(defn- field-to-str [f]
+  (format "%s %s %s"
+          (str/join " " (map name (into [] (get f :flags))))
+          (get f :type)
+          (get f :name)))
+
+(defn- method-to-str [f]
+  (format "%s %s %s(%s)"
+          (str/join " " (map name (into [] (get f :flags))))
+          (get f :return-type)
+          (get f :name)
+          (str/join ", " (get f :parameter-types))))
+
+(defn- print-all-members [members]
+  (print-members members "Static fields" field-to-str
+                 (every-pred field? static?))
+  (print-members members "Static methods" method-to-str
+                 (every-pred method? static?))
+  (println)
+  (print-members members "Instance fields" field-to-str
+                 (every-pred field? (complement static?)))
+  (print-members members "Instance methods" method-to-str
+                 (every-pred method? (complement static?))))
 
 (defn -main
-  [& args]
-  (pprint/write (get-classes "java.lang")))
+  [cls & _]
+  (try
+    (->> cls
+         (Class/forName)
+         (cr/reflect)
+         (:members)
+         (print-all-members))
+    (catch Exception e
+      (println
+        (str/join ""
+                  ["Error: "
+                   (->> e (.getClass) (.getName))
+                   ": "
+                   (.getMessage e)])))))
