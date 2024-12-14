e73fe61159ccfe6d5cce3903b3afbcfa6ceaf412
Albert Shefner <dev_n0emo@tuta.io>
2024-12-14 12:54:50 +0300

    5/Java/practice enable AOT compilation and embed Jetty

--- Term5/Java/practice/project.clj
+++ Term5/Java/practice/project.clj
@@ -2,19 +2,24 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]
-                 [dev.weavejester/ragtime "0.10.1"]
-                 [org.postgresql/postgresql "42.7.4"]
-                 [hiccup "2.0.0-RC4"]
+  :dependencies [[com.h2database/h2 "2.3.232"]
                  [com.layerware/hugsql "0.5.3"]
-                 [ring/ring-json "0.5.1"]
-                 [com.h2database/h2 "2.3.232"]]
+                 [compojure "1.6.1"]
+                 [dev.weavejester/ragtime "0.10.1"]
+                 [hiccup "2.0.0-RC4"]
+                 [org.clojure/clojure "1.10.0"]
+                 [org.postgresql/postgresql "42.7.4"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.1"]]
+  :aot [students.core]
   :plugins [[lein-ring "0.12.5"]]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :ring {:handler students.handler/app}
   :aliases {"migrate"  ["run" "-m" "user/migrate"]
             "rollback" ["run" "-m" "user/rollback"]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+
+                        [ring/ring-mock "0.3.2"]]}
+   :uberjar {:aot :all
+             :main students.core}})
