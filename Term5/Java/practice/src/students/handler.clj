6770b70d5aa0a4ffbe6b86c64d633a3a92ef9b3b
Albert Shefner <dev_n0emo@tuta.io>
2024-12-18 17:00:35 +0300

    5/Java/practice implement session auth

--- Term5/Java/practice/src/students/handler.clj
+++ Term5/Java/practice/src/students/handler.clj
@@ -1,32 +1,36 @@
 (ns students.handler
   (:require
-   [compojure.core :refer [defroutes GET]]
+   [compojure.core :refer [defroutes GET routes wrap-routes]]
    [compojure.route :as route]
    [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+   [ring.middleware.session.cookie :refer [cookie-store]]
    [students.pages.index :as index]
    [students.pages.not-found :as not-found]
-   [students.routes.api :refer [api-routes]]
    [students.routes.disciplines :refer [disciplines-routes]]
    [students.routes.faculties :refer [faculties-routes]]
    [students.routes.groups :refer [groups-routes]]
+   [students.routes.login :refer [login-routes wrap-protected]]
    [students.routes.marks :refer [marks-routes]]
    [students.routes.programs :refer [programs-routes]]
    [students.routes.students :refer [students-routes]]))
 
 (defroutes app-routes
-  faculties-routes
-  disciplines-routes
-  programs-routes
-  groups-routes
-  students-routes
-  marks-routes
-  api-routes
-  (GET "/" [] (index/render))
+  login-routes
+  (-> (routes
+        faculties-routes
+        disciplines-routes
+        programs-routes
+        groups-routes
+        students-routes
+        marks-routes
+        (GET "/" [] (index/render)))
+      (wrap-routes wrap-protected))
   (route/resources "/")
   (route/not-found (not-found/render)))
 
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (def app
   (wrap-defaults
    app-routes
-   site-defaults))
+   (-> site-defaults
+       (assoc-in [:session :store] (cookie-store))
+       (assoc-in [:session :cookie-attrs] {:max-age 3600}))))
