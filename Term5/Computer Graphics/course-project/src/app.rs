a2a332859812d89919d8c6207963a6f48b24f0ff
Albert Shefner <solaris945@gmail.com>
2025-01-09 00:57:00 +0300

    5/CG Reformat all

--- Term5/Computer Graphics/course-project/src/app.rs
+++ Term5/Computer Graphics/course-project/src/app.rs
@@ -1,5 +1,10 @@
 use framework::WgpuContext;
-use wgpu::{util::{BufferInitDescriptor, DeviceExt as _}, BlendState, ColorTargetState, ColorWrites, CommandEncoderDescriptor, FragmentState, Operations, PipelineCompilationOptions, PrimitiveState, RenderPassColorAttachment, RenderPassDescriptor, RenderPipelineDescriptor, VertexAttribute, VertexBufferLayout, VertexState, VertexStepMode};
+use wgpu::{
+    util::{BufferInitDescriptor, DeviceExt as _},
+    BlendState, ColorTargetState, ColorWrites, CommandEncoderDescriptor, FragmentState, Operations,
+    PipelineCompilationOptions, PrimitiveState, RenderPassColorAttachment, RenderPassDescriptor,
+    RenderPipelineDescriptor, VertexAttribute, VertexBufferLayout, VertexState, VertexStepMode,
+};
 
 pub struct Application {
     pipeline: wgpu::RenderPipeline,
@@ -9,18 +14,21 @@ pub struct Application {
 impl framework::Application for Application {
     const NAME: &str = "КП. Ray marching";
 
-    const DESCRIPTION: &str = "Курсовой проект - реализация трёхмерной сцены с помощью Ray marching";
+    const DESCRIPTION: &str =
+        "Курсовой проект - реализация трёхмерной сцены с помощью Ray marching";
 
     fn init(config: &wgpu::SurfaceConfiguration, ctx: &WgpuContext) -> Self {
-        let shader = ctx.device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        let shader = ctx
+            .device
+            .create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
 
         let vertices: [Vertex; 6] = [
-            Vertex { pos: [ 1.0,  1.0] },
-            Vertex { pos: [-1.0,  1.0] },
+            Vertex { pos: [1.0, 1.0] },
+            Vertex { pos: [-1.0, 1.0] },
             Vertex { pos: [-1.0, -1.0] },
             Vertex { pos: [-1.0, -1.0] },
-            Vertex { pos: [ 1.0, -1.0] },
-            Vertex { pos: [ 1.0,  1.0] },
+            Vertex { pos: [1.0, -1.0] },
+            Vertex { pos: [1.0, 1.0] },
         ];
 
         let vertex_buf = ctx.device.create_buffer_init(&BufferInitDescriptor {
@@ -29,57 +37,68 @@ impl framework::Application for Application {
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let pipeline =
+            ctx.device
+                .create_render_pipeline(&RenderPipelineDescriptor {
+                    label: Some("Ray Marching Render Pipeline"),
+                    layout: Some(&ctx.device.create_pipeline_layout(
+                        &wgpu::PipelineLayoutDescriptor {
+                            label: Some("Ray MArching Pipeline Layout"),
+                            bind_group_layouts: &[],
+                            push_constant_ranges: &[],
+                        },
+                    )),
+                    vertex: VertexState {
+                        module: &shader,
+                        entry_point: Some("vs_main"),
+                        compilation_options: PipelineCompilationOptions::default(),
+                        buffers: &[Vertex::layout()],
+                    },
+                    fragment: Some(FragmentState {
+                        module: &shader,
+                        entry_point: Some("fs_main"),
+                        targets: &[Some(ColorTargetState {
+                            format: config.view_formats[0],
+                            blend: Some(BlendState::ALPHA_BLENDING),
+                            write_mask: ColorWrites::ALL,
+                        })],
+                        compilation_options: PipelineCompilationOptions::default(),
+                    }),
+                    primitive: PrimitiveState {
+                        topology: wgpu::PrimitiveTopology::TriangleList,
+                        strip_index_format: None,
+                        front_face: wgpu::FrontFace::Ccw,
+                        cull_mode: Some(wgpu::Face::Back),
+                        polygon_mode: wgpu::PolygonMode::Fill,
+                        unclipped_depth: false,
+                        conservative: false,
+                    },
+                    depth_stencil: None,
+                    multisample: wgpu::MultisampleState {
+                        count: 1,
+                        mask: !0,
+                        alpha_to_coverage_enabled: false,
+                    },
+                    multiview: None,
+                    cache: None,
+                });
 
-        let pipeline = ctx.device.create_render_pipeline(&RenderPipelineDescriptor {
-            label: Some("Ray Marching Render Pipeline"),
-            layout: Some(&ctx.device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Ray MArching Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            })),
-            vertex: VertexState {
-                module: &shader,
-                entry_point: Some("vs_main"),
-                compilation_options: PipelineCompilationOptions::default(),
-                buffers: &[Vertex::layout()],
-            },
-            fragment: Some(FragmentState {
-                module: &shader,
-                entry_point: Some("fs_main"),
-                targets: &[Some(ColorTargetState {
-                    format: config.view_formats[0],
-                    blend: Some(BlendState::ALPHA_BLENDING),
-                    write_mask: ColorWrites::ALL,
-                })],
-                compilation_options: PipelineCompilationOptions::default(),
-            }),
-            primitive: PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                polygon_mode: wgpu::PolygonMode::Fill,
-                unclipped_depth: false,
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-            cache: None,
-        });
-
-        Self { pipeline, vertex_buf }
+        Self {
+            pipeline,
+            vertex_buf,
+        }
     }
 
     fn render(
         &mut self,
         view: &wgpu::TextureView,
         _dt: f32,
-        WgpuContext { instance: _, adapter: _, device, queue }: &WgpuContext
+        WgpuContext {
+            instance: _,
+            adapter: _,
+            device,
+            queue,
+        }: &WgpuContext,
     ) {
         let mut encoder = device.create_command_encoder(&CommandEncoderDescriptor {
             label: Some("Ray Marching Command Encoder"),
@@ -122,8 +141,7 @@ pub struct Vertex {
 }
 
 impl Vertex {
-    const ATTRIBUTES: &'static [VertexAttribute] =
-        &wgpu::vertex_attr_array![0 => Float32x2];
+    const ATTRIBUTES: &'static [VertexAttribute] = &wgpu::vertex_attr_array![0 => Float32x2];
 
     pub fn layout() -> VertexBufferLayout<'static> {
         VertexBufferLayout {
