201234d2d278f2fe20ccad214011ee094c40c8f3
Albert Shefner <dev_n0emo@tuta.io>
2025-01-10 20:36:52 +0300

    5/CG finalize lab3 and fix android

--- Term5/Computer Graphics/lab3/src/lib.rs
+++ Term5/Computer Graphics/lab3/src/lib.rs
@@ -1,13 +1,11 @@
 mod app;
 mod camera;
+mod color;
 mod instance;
 mod light;
 mod material;
 mod model;
 mod render;
 mod texture;
-mod color;
 
 pub use app::Application;
-
-const SAMPLE_COUNT: u32 = 4;
