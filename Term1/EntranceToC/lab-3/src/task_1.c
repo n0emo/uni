2e2b0208722fe0594d2c3e29c2552fe6a1b21870
Albert Shefner <usn0emo@gmail.com>
2026-04-22 23:52:08 +0300

    1/C/3 Write report

--- Term1/EntranceToC/lab-3/src/task_1.c
+++ Term1/EntranceToC/lab-3/src/task_1.c
@@ -3,16 +3,14 @@
 
 int main()
 {
-    double m, x, F, T, dF, Fk;
+    double m = 10;
+    double x = 0.15;
+    double dF = 0.15;
+    double Fk = 3.2;
 
-    m  = 10;
-    x  = 0.15;
-    dF = 0.15;
-    Fk = 3.2;
-
-    for (F = 1.85; F < Fk + dF * 0.5; F += dF)
+    for (double F = 1.85; F < Fk + dF * 0.5; F += dF)
     {
-        T = 2 * M_PI * sqrt(m * x / F);
+        double T = 2 * M_PI * sqrt(m * x / F);
         printf("F = %lf, T = %lf\n", F, T);
     }
 
